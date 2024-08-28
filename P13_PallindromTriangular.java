6e536a60d7dac11474845a613b0de8a6561373f3
Anjarul347 <skanjarulhossain347@gmail.com>
2024-08-28 18:15:28 +0530

    Pallindrom Triangular

--- P13_PallindromTriangular.java
+++ P13_PallindromTriangular.java
@@ -1,3 +1,10 @@
+/*
+                 1
+               2 1 2
+             3 2 1 2 3
+           4 3 2 1 2 3 4
+  */
+
 
 import java.util.Scanner;
 public class P13_PallindromTriangular {
@@ -7,6 +14,24 @@ public class P13_PallindromTriangular {
         System.out.println("Enter the number of row and column:-");
         int n=sc.nextInt();
 
-        
+        for(int i=1;i<=n;i++){
+            //Printing the Space
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+
+            for(int k=i;k>=1;k--){
+                System.out.print(k + " ");
+            }
+
+            for(int k=2;k<=i;k++){
+                System.out.print(k+" ");
+            }
+                    
+            
+            System.out.println();
+
+        }
+
     }
 }
