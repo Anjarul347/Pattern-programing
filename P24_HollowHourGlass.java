6802ffef4ed56f2a822dc6a09687795258751bd5
Anjarul347 <skanjarulhossain347@gmail.com>
2024-09-09 23:48:09 +0530

    Hollow hour glass

---
+++ P24_HollowHourGlass.java
@@ -0,0 +1,40 @@
+
+import java.util.Scanner;
+public class P24_HollowHourGlass {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of row:-");
+
+        int n=sc.nextInt();
+
+        int p=n;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<i;j++){
+                System.out.print(" ");
+            }
+            System.out.print("*");
+            
+            
+            if(i==1){
+                for(int k=1;k<n;k++){
+                    System.out.print(" *");
+                }
+            }
+
+            if(i>1 && i<n){
+            for(int j=1;j<=p;j++){
+                System.out.print(" ");
+               
+            }
+            System.out.print("*");
+            p-=2;
+            }
+
+            System.out.println();
+            
+        }
+
+
+
+    }
+}
