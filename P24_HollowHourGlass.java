2f08e7a82a62b03c50f853e090c44c4da8cb4182
Anjarul347 <skanjarulhossain347@gmail.com>
2024-09-10 19:34:49 +0530

    Hollow Hour Glass

--- P24_HollowHourGlass.java
+++ P24_HollowHourGlass.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 public class P24_HollowHourGlass {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        System.out.println("Enter the number of row:-");
+
+        System.out.println("Enter any number to generate Hollow diamond pattern:-");
 
         int n=sc.nextInt();
-
-        int p=n;
+        
+        int p=((n-3)*2)+1;
         for(int i=1;i<=n;i++){
             for(int j=1;j<i;j++){
                 System.out.print(" ");
@@ -33,6 +34,33 @@ public class P24_HollowHourGlass {
             System.out.println();
             
         }
+
+        
+        for(int i=1;i<n;i++){
+            for(int s=i;s<n-1;s++){
+                System.out.print(" ");
+            }
+            System.out.print("*");
+
+           if(i<n-1){
+            p+=2;
+            for(int j=1;j<=p;j++){
+                System.out.print(" ");
+            }
+            System.out.print("*");
+           }
+
+            if(i==n-1){
+                for(int k=1;k<n;k++){
+                    System.out.print(" *");
+                }
+            }
+            System.out.println();
+        }
+
+
+
+       
 
 
 
